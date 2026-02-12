00f9a9a9637c1dc20951f35fd179ceb2cf6a2601
Sumit prasad <sumitprasadd.wb24@gmail.com>
2026-02-12 21:27:17 +0530

    prepare base model and create pipeline

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from ChestCancerClassification import logger
 from ChestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChestCancerClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -11,3 +14,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
